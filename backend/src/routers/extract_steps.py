e0cc8a892d4b3aa0743f0cf6233981733319a1a1
273* <keita.y310@icloud.com>
2026-01-07 15:03:24 +0000

    feat: xmlファイルを受け取るリクエストメソッドを実装

--- backend/src/routers/extract_steps.py
+++ backend/src/routers/extract_steps.py
@@ -1,8 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 
-router = APIRouter()
+router = APIRouter(prefix="/extract-steps", tags=["extract-steps"])
 
 
-@router.post("/extract-steps/")
-def extract_steps_from_applehealthcare():
-    return {"message": "Extracted steps from Apple Healthcare data."}
+@router.post(
+    "",
+    summary="歩数データの抽出",
+    description="apple healthcareデータから歩数を抽出します。",
+    responses={
+        200: {
+            "description": "歩数データの抽出が完了",
+        }
+    },
+)
+async def extract_steps(
+    xml_data: str = Body(
+        media_type="text/xml",
+        description="Apple Healthcareからエクスポートしたzipファイルを解凍した時に取得できるXMLファイル(export.xml)",
+    ),
+):
+    """歩数データの抽出を受け付ける"""
+    return {
+        "message": "Extracted steps from Apple Healthcare data.",
+        "size": len(xml_data),
+    }
--- backend/src/schemas/steps.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Steps(BaseModel):
-    data: list[str]
