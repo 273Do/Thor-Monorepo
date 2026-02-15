f2b4d934792cfca22c4630ce4fd10c60b8c84c45
273do <keita.y310@icloud.com>
2026-02-15 09:55:08 +0000

    feat: reaft router fwからreactへ移行

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
-import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
   server: {
     host: "0.0.0.0",
     port: 5173,
